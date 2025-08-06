960eafc99469cc0d294d1b9abc1d5e1e94e2bb83
piyuscoder <piyushks4121@gmail.com>
2025-08-06 12:36:52 +0530

    main.c
    c file

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+    int n, min = 1, max = 100, count = 5;
+    srand(time(0));
+    int randomNumber = min + rand() % (max - min + 1);
+
+
+    printf("Enter number between 1 to 100\n");
+    printf("<----------------you have only 5 chance to guess the correct number-------------->\n");
+
+    while (count)
+    {
+        printf("Enter a number : ");
+        scanf("%d", &n);
+        if (randomNumber == n)
+        {
+            printf("\nCongratulations you won 7 CRORE \n\n");
+            return 0;
+        }
+        else if (randomNumber > n)
+        {
+            printf("Please enter greater number \n");
+        }
+        else
+        {
+            printf("Please enter smaller number \n");
+        }
+        count--;
+    }
+    printf("\nYou lost the game. Try again...");
+    return 0;
+}
